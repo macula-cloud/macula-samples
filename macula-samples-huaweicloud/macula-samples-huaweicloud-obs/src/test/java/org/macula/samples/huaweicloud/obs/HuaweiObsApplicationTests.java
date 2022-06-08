1eef6c748971434048ff7975c81d1d96200a819e
jokeway <jokeway@gmail.com>
2022-06-08 14:35:05 +0800

    feat: update jdk 17

--- macula-samples-huaweicloud/macula-samples-huaweicloud-obs/src/test/java/org/macula/samples/huaweicloud/obs/HuaweiObsApplicationTests.java
+++ macula-samples-huaweicloud/macula-samples-huaweicloud-obs/src/test/java/org/macula/samples/huaweicloud/obs/HuaweiObsApplicationTests.java
@@ -53,25 +53,38 @@ public class HuaweiObsApplicationTests {
 		for (int i = 0; i < 20; i++) {
 			executorService.execute(() -> {
 				log.info("Thread [{}] turns running!", Thread.currentThread().getName());
-				String objectKey = null;
-				while (!StringUtils.equals(objectKey, "<EOF>")) {
+				String token = null;
+				while (!StringUtils.equals(token, "<EOF>")) {
 					try {
-						objectKey = queue.take();
+						token = queue.take();
 					} catch (InterruptedException e) {
 						break;
 					}
-					if (!StringUtils.equals(objectKey, "<EOF>")) {
+					if (!StringUtils.equals(token, "<EOF>")) {
+						String[] keys = token.split("\\^");
+						int id = Integer.valueOf(keys[0]);
+						String obsKey = keys[1];
+						String comments;
+
 						try (ByteArrayOutputStream output = new ByteArrayOutputStream()) {
-							webdavClient.download(objectKey, output);
+							webdavClient.download(obsKey, output);
 							if (output.size() > 0) {
-								obsClient.putObject(obsConfig.getBucket(), objectKey, new ByteArrayInputStream(output.toByteArray()));
-								log.info("file: {} complete!", objectKey);
+								obsClient.putObject(obsConfig.getBucket(), obsKey, new ByteArrayInputStream(output.toByteArray()));
+								log.info("file: {} complete!", token);
+								comments = "OK";
 							} else {
-								log.error("file: {} not exists!", objectKey);
+								log.error("file: {} not exists!", token);
+								comments = "NotExists";
 							}
 						} catch (IOException e) {
 							e.printStackTrace();
+							comments = "ERROR";
 						}
+
+						jdbcTemplate.update(
+								"insert into sync_obs_result values(?, ?, ?) ON DUPLICATE KEY UPDATE obs_key = VALUES(obs_key), comments = VALUES(comments) ",
+								id, obsKey, comments);
+
 					}
 				}
 				countDownLatch.countDown();
@@ -82,34 +95,37 @@ public class HuaweiObsApplicationTests {
 
 	@Test
 	public void testUploadFile() throws IOException, InterruptedException {
-		int id = 0, size = 0;
+		int startPos = 0;
+		try {
+			startPos = jdbcTemplate.queryForObject("select max(id) from sync_obs_result", Integer.class);
+		} catch (Exception ex) {
+			// IGNORE
+		}
+		int size = 0;
 		do {
-			log.info("=================== from {} limit 100 ==================", id);
+			log.info("=================== from {} limit 100 ==================", startPos);
 			List<Map<String, Object>> result = jdbcTemplate
-					.queryForList("select id, server_path, file_name from sync_bos_file_list_a where id > ? order by id limit 100", id);
+					.queryForList("select id, server_path, file_name from sync_bos_file_list_a where id > ? order by id limit 100", startPos);
 			for (Map<String, Object> map : result) {
-				Integer cId = (Integer) map.get("id");
-				String category = (String) map.get("server_path");
+				Integer rowId = (Integer) map.get("id");
+				String path = (String) map.get("server_path");
 				String fileName = (String) map.get("file_name");
 				// String category = "\\iplatform\\72f337e1\\2a3e\\46ab\\bf5d\\4a5b3c0a3a52";
 				// String file = "A084811935FE86B6CE79A8BA7A85154D40247FC6-44278.docx";
-				if (cId > id) {
-					id = cId;
+				if (rowId > startPos) {
+					startPos = rowId;
 				}
-				String file = WebdavUtils.getFormatPath(category) + SymbolConstants.SLASH + fileName;
-				log.info("id -> {} add file: {}", cId, file);
-				queue.put(file);
+				String url = WebdavUtils.getFormatPath(path) + SymbolConstants.SLASH + fileName;
+				log.info("id -> {} add file: {}", rowId, url);
+				queue.put(rowId + "^" + url);
 			}
 			size = result.size();
-			Thread.sleep(1000);
-		} while (size >= 100);
+			Thread.sleep(200);
+		} while (size > 0);
+
 		while (!countDownLatch.await(1000, TimeUnit.MILLISECONDS)) {
-			try {
-				log.info("Watiing threads [{}] complete.", countDownLatch.getCount());
-				queue.put("<EOF>");
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+			log.info("Waiting threads [{}] complete.", countDownLatch.getCount());
+			queue.put("<EOF>");
 		}
 	}
 
